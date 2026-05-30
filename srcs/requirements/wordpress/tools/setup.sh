10d88b317483971960c364cb9b2db78fe3f0f4c6
Daniel Gomez Arango <dgomez-a@student.42berlin.de>
2026-05-30 23:32:40 +0200

    refactor: wp-cli commands in a function

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -2,17 +2,21 @@
 
 chown -R www-data:www-data /var/www/inception/
 
+wp_cli() {
+  php -d memory_limit=512M /usr/local/bin/wp --allow-root --path="/var/www/inception/" "$@"
+}
+
 if [ ! -f "/var/www/inception/wp-config.php" ]; then
   mv /tmp/wp-config.php /var/www/inception/
 fi
 
 sleep 10
 
-wp --allow-root --path="/var/www/inception/" core download || true
+wp_cli core download || true
 
-if ! wp --allow-root --path="/var/www/inception/" core is-installed;
+if ! wp_cli core is-installed;
 then
-  wp  --allow-root --path="/var/www/inception/" core install \
+  wp_cli core install \
       --url=$WP_URL \
       --title=$WP_TITLE \
       --admin_user=$WP_ADMIN_USER \
@@ -20,15 +24,15 @@ then
       --admin_email=$WP_ADMIN_EMAIL
 fi;
 
-if ! wp --allow-root --path="/var/www/inception/" user get $WP_USER;
+if ! wp_cli user get $WP_USER;
 then
-  wp  --allow-root --path="/var/www/inception/" user create \
+  wp_cli user create \
       $WP_USER \
       $WP_EMAIL \
       --user_pass=$WP_PASSWORD \
       --role=$WP_ROLE
 fi;
 
-wp --allow-root --path="/var/www/inception/" theme install raft --activate 
+wp_cli theme install raft --activate 
 
 exec $@
