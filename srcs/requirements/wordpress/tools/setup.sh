1de632763990ca7a317a52b0787e764ff5687c54
Daniel Gomez Arango <dgomez-a@student.42berlin.de>
2026-06-08 14:00:09 +0200

    fix: retrieve credentials in wp setup

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -29,20 +29,41 @@ wp_cli core download || true
 if ! wp_cli core is-installed;
 then
   wp_cli core install \
-      --url=$WP_URL \
-      --title=$WP_TITLE \
-      --admin_user=$WP_ADMIN_USER \
-      --admin_password=$WP_ADMIN_PASSWORD \
-      --admin_email=$WP_ADMIN_EMAIL
+      --url="$WP_URL" \
+      --title="$WP_TITLE" \
+      --admin_user="$WP_ADMIN_USER" \
+      --admin_password="$WP_ADMIN_PASSWORD" \
+      --admin_email="$WP_ADMIN_EMAIL"
 fi;
 
-if ! wp_cli user get $WP_USER;
+if wp_cli user get $WP_ADMIN_USER;
 then
+  wp_cli user update \
+      "$WP_ADMIN_USER" \
+      --user_email="$WP_ADMIN_EMAIL" \
+      --user_pass="$WP_ADMIN_PASSWORD" \
+      --role=administrator
+else
   wp_cli user create \
-      $WP_USER \
-      $WP_EMAIL \
-      --user_pass=$WP_PASSWORD \
-      --role=$WP_ROLE
+  "$WP_ADMIN_USER" \
+  "$WP_ADMIN_EMAIL" \
+  --user_pass="$WP_ADMIN_PASSWORD" \
+  --role=administrator
+fi;
+
+if wp_cli user get "$WP_USER";
+then
+  wp_cli user update \
+      "$WP_USER" \
+      --user_email="$WP_EMAIL" \
+      --user_pass="$WP_PASSWORD" \
+      --role="$WP_ROLE"
+else
+  wp_cli user create \
+      "$WP_USER" \
+      "$WP_EMAIL" \
+      --user_pass="$WP_PASSWORD" \
+      --role="$WP_ROLE"
 fi;
 
 wp_cli theme install raft --activate 
